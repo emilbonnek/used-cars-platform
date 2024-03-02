715ec11c008004c7316e667fb5371f4305842fa3
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-02 01:31:43 +0100

    Create user.ts

---
+++ src/api/user.ts
@@ -0,0 +1,18 @@
+import { Output, email, object, parse, string } from "valibot";
+
+export const UserSchema = object({
+  email: string([email()]),
+  firstName: string(),
+  lastName: string(),
+});
+export type User = Output<typeof UserSchema>;
+
+/**
+ * Get the current user from the data/user.json file
+ */
+export async function getUser(): Promise<User> {
+  const response = await fetch("/data/user.json");
+  const json = await response.json();
+  const user = parse(UserSchema, json);
+  return user;
+}
