e95381007bd8b13d9398f6e1d3306cd2b2fbd9f4
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-02 03:00:08 +0100

    update / route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,23 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { getUser } from "../api/user";
+import { getListings } from "../api/listings";
 
 export const Route = createFileRoute("/")({
   component: Index,
   loader: async () => {
-    const user = await getUser();
-    return { user };
+    const [user, listings] = await Promise.all([getUser(), getListings()]);
+    const listingsCount = listings.length;
+    return { user, listingsCount };
   },
 });
 
 function Index() {
-  const { user } = Route.useLoaderData();
+  const { user, listingsCount } = Route.useLoaderData();
 
   return (
     <div>
       <h1>Hello {user.firstName}!</h1>
+      <p>There is {listingsCount} listings.</p>
     </div>
   );
 }
