30f54aae01dd8858c890a7860be1669472d404fe
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-04 00:10:49 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,13 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { LISTING_MAX_PRICE, LISTING_MIN_PRICE } from "../api/listings";
+import { NavMain } from "../components/NavMain";
+import { NavigateHome } from "../components/NavigateHome";
 
 export const Route = createRootRoute({
+  notFoundComponent: NavigateHome,
   component: () => (
     <>
-      <Link to="/">Home</Link>
-      <Link
-        to="/listings"
-        search={{
-          pageSize: 10,
-          page: 1,
-          minPrice: LISTING_MIN_PRICE,
-          maxPrice: LISTING_MAX_PRICE,
-          minKilometersDriven: LISTING_MIN_PRICE,
-          maxKilometersDriven: LISTING_MAX_PRICE,
-        }}
-      >
-        Listings
-      </Link>
+      <NavMain />
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
