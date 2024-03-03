34d7f8ed013b660087ab7bdc041d72b1acf59d2f
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-04 00:11:41 +0100

    Update listings_.$id.tsx

--- src/routes/listings_.$id.tsx
+++ src/routes/listings_.$id.tsx
@@ -1,9 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { getListing } from "../api/listings";
 import { CardListing } from "../components/CardListing";
+import { Spinner } from "../components/Spinner";
 
 export const Route = createFileRoute("/listings/$id")({
   component: ListingId,
+  pendingComponent: Spinner,
   loader: async ({ params, abortController }) => {
     return getListing(params.id, abortController.signal);
   },
