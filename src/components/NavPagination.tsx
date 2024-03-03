2a810b7737dd99d4ef5e251c4c017e2a30709729
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-04 00:11:36 +0100

    Update NavPagination.tsx

--- src/components/NavPagination.tsx
+++ src/components/NavPagination.tsx
@@ -1,40 +1,66 @@
-import { Link } from "@tanstack/react-router";
+import { Await, DeferredPromise, Link } from "@tanstack/react-router";
+import { Suspense } from "react";
 
 interface NavPaginationProps {
+  pageSize: number;
   page: number;
-  pageCount: number;
+  defferedElementsCount: DeferredPromise<number>;
 }
 
-export function NavPagination({ page, pageCount }: NavPaginationProps) {
+export function NavPagination({
+  pageSize,
+  page,
+  defferedElementsCount,
+}: NavPaginationProps) {
   return (
-    <nav className="flex gap-2">
-      {page > 1 && (
+    <nav className="grid grid-cols-3 items-center justify-items-center gap-4 py-4">
+      {page > 1 ? (
         <Link
           from="/listings"
           search={(prev) => ({
             ...prev,
             page: prev.page - 1,
           })}
+          className="focus:shadow-outline justify-self-end rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
         >
           Previous
         </Link>
+      ) : (
+        <div></div> // Empty div to keep the grid structure
       )}
 
-      <span>
-        Page {page} of {pageCount}
+      <span className="text-sm font-semibold sm:text-base">
+        Page {page} of{" "}
+        <Suspense fallback={<span>...</span>}>
+          <Await promise={defferedElementsCount}>
+            {(elementsCount) => {
+              const pageCount = Math.ceil(elementsCount / pageSize);
+              return pageCount;
+            }}
+          </Await>
+        </Suspense>
       </span>
 
-      {page < pageCount && (
-        <Link
-          from="/listings"
-          search={(prev) => ({
-            ...prev,
-            page: prev.page + 1,
-          })}
-        >
-          Next
-        </Link>
-      )}
+      <Suspense fallback={<div></div>}>
+        {" "}
+        {/* Empty div for alignment if Next is not rendered */}
+        <Await promise={defferedElementsCount}>
+          {(elementsCount) =>
+            page < Math.ceil(elementsCount / pageSize) && (
+              <Link
+                from="/listings"
+                search={(prev) => ({
+                  ...prev,
+                  page: prev.page + 1,
+                })}
+                className="focus:shadow-outline justify-self-start rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 focus:outline-none"
+              >
+                Next
+              </Link>
+            )
+          }
+        </Await>
+      </Suspense>
     </nav>
   );
 }
