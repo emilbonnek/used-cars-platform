b2cf94010a7fd72cc5d919fd6ef2a1429e0a789b
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-04 00:11:01 +0100

    Update listings.tsx

--- src/routes/listings.tsx
+++ src/routes/listings.tsx
@@ -1,43 +1,80 @@
-import { createFileRoute, useNavigate } from "@tanstack/react-router";
-import { ListingSearch, getListings } from "../api/listings";
+import {
+  Await,
+  createFileRoute,
+  defer,
+  useNavigate,
+} from "@tanstack/react-router";
+import { ListingFilter, getListings } from "../api/listings";
 import { CardListing } from "../components/CardListing";
 import { Output, merge, parse } from "valibot";
 import { FormListingSearch } from "../components/FormListingSearch";
 import { Paginate } from "../pagination";
 import { FormPageSize } from "../components/FormPageSize";
+import { Spinner } from "../components/Spinner";
+import { Suspense } from "react";
 import { NavPagination } from "../components/NavPagination";
 
-const ListingsSearchAndPaginate = merge([ListingSearch, Paginate]);
-type ListingsSearchAndPaginate = Output<typeof ListingsSearchAndPaginate>;
+const ListingsFilterAndPaginate = merge([ListingFilter, Paginate]);
+type ListingsFilterAndPaginate = Output<typeof ListingsFilterAndPaginate>;
 
 export const Route = createFileRoute("/listings")({
   component: Listings,
+  pendingComponent: Spinner,
   validateSearch: (search: Record<string, unknown>) =>
-    parse(ListingsSearchAndPaginate, search),
+    parse(ListingsFilterAndPaginate, search),
   loaderDeps: ({ search }) => search,
-  loader: async ({ deps: { pageSize, page, ...search }, abortController }) => {
-    const listings = await getListings(search, abortController.signal);
+  loader: ({
+    deps: { pageSize, page, ...listingSearch },
+    abortController,
+    navigate,
+  }) => {
+    const listings = getListings(listingSearch, abortController.signal);
 
-    const start = (page - 1) * pageSize;
-    const end = start + pageSize;
-    const paginatedListings = listings.slice(start, end);
-    const pageCount = Math.ceil(listings.length / pageSize);
+    listings.then((listings) => {
+      const listingsCount = listings.length;
+      const pageCount = Math.ceil(listingsCount / pageSize);
+      if (page > pageCount) {
+        navigate({
+          search: {
+            ...listingSearch,
+            pageSize,
+            page: pageCount,
+          },
+        });
+      }
+    });
+
+    const defferedPaginatedListings = defer(
+      listings.then((listings) => {
+        const start = (page - 1) * pageSize;
+        const end = start + pageSize;
+        return listings.slice(start, end);
+      }),
+    );
+
+    const defferedListingsCount = defer(
+      listings.then((listings) => {
+        return listings.length;
+      }),
+    );
 
     return {
-      listings: paginatedListings,
-      pageCount,
+      defferedListings: defferedPaginatedListings,
+      defferedListingsCount,
     };
   },
 });
 
 function Listings() {
   const navigate = useNavigate({ from: Route.fullPath });
-  const { listings, pageCount } = Route.useLoaderData();
+  const { defferedListings, defferedListingsCount } = Route.useLoaderData();
   const { pageSize, page, ...search } = Route.useSearch();
 
+  console.log(page);
+
   return (
-    <div className="p-2">
-      <div className="flex justify-between">
+    <div>
+      <div className="flex flex-col justify-between gap-4 bg-slate-100 px-2 py-6 md:flex-row">
         <FormListingSearch
           search={search}
           onSubmit={(newSearch) => {
@@ -65,16 +102,39 @@ function Listings() {
         />
       </div>
 
-      <div className="container grid gap-6 md:gap-8 px-4 md:px-6">
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {listings.map((listing) => (
-            <CardListing key={listing.id} listing={listing} />
-          ))}
-        </div>
+      <div className="container mx-auto px-4 py-4 md:px-6">
+        <Suspense fallback={<Spinner />}>
+          <Await promise={defferedListingsCount}>
+            {(listingsCount) => {
+              return (
+                <div className="my-4">
+                  <p className="text-gray-600">
+                    Showing {listingsCount} listings
+                  </p>
+                </div>
+              );
+            }}
+          </Await>
+          <div className="grid gap-6 md:gap-8">
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+              <Await promise={defferedListings}>
+                {(listings) => {
+                  return listings.map((listing) => (
+                    <CardListing key={listing.id} listing={listing} />
+                  ));
+                }}
+              </Await>
+            </div>
+          </div>
+        </Suspense>
       </div>
 
       <div className="flex justify-center">
-        <NavPagination page={page} pageCount={pageCount} />
+        <NavPagination
+          pageSize={pageSize}
+          page={page}
+          defferedElementsCount={defferedListingsCount}
+        />
       </div>
     </div>
   );
