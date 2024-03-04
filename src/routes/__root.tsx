be8f9dd4de32822ac9de71783bc87d69ceeaaa78
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2024-03-04 16:12:00 +0100

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,28 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { NavMain } from "../components/NavMain";
 import { NavigateHome } from "../components/NavigateHome";
+import React, { Suspense } from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        })),
+      );
 
 export const Route = createRootRoute({
   notFoundComponent: NavigateHome,
+  pendingComponent: () => <></>,
   component: () => (
     <>
       <NavMain />
       <Outlet />
-      <TanStackRouterDevtools />
+      <Suspense>
+        <TanStackRouterDevtools />
+      </Suspense>
     </>
   ),
 });
